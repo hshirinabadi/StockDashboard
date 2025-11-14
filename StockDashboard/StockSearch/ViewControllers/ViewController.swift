d97cd70b534ec60850a8def6c5d896d2368b7fd2
Hossein Shirinabadi <hosseinshirinabadi@Mac.lan>
2025-11-14 10:44:02 -0500

    creare skeleton - add gitignore

---
+++ StockDashboard/StockSearch/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StockSearchViewController.swift
+//  StockDashboard
+//
+//  Created by Hossein Shirinabadi on 11/14/25.
+//
+
+import UIKit
+
+class StockSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
